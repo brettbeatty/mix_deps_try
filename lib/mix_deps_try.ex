71d9ba79e6a0b59df5468350844f5fb9d8121698
Brett Beatty <brettbeatty@gmail.com>
2020-09-15 17:44:12 -0600

    Generate new project

---
+++ lib/mix_deps_try.ex
@@ -0,0 +1,18 @@
+defmodule MixDepsTry do
+  @moduledoc """
+  Documentation for `MixDepsTry`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MixDepsTry.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mix_deps_try_test.exs
@@ -0,0 +1,8 @@
+defmodule MixDepsTryTest do
+  use ExUnit.Case
+  doctest MixDepsTry
+
+  test "greets the world" do
+    assert MixDepsTry.hello() == :world
+  end
+end
